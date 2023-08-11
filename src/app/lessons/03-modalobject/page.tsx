d2078791c5df3eacdb8eb842445ea657e7bd42e4
0xmichael <798906172@qq.com>
2023-08-11 18:13:40 +0800

    modal object and material

--- src/app/lessons/03-modalobject/page.tsx
+++ src/app/lessons/03-modalobject/page.tsx
@@ -79,9 +79,48 @@ const BufferGeometry = () => {
     color.set("#00ff00");
     console.log("查看颜色对象结构：", color);
 
+    // 模型材质
+    material.transparent = true; // 开启透明
+    material.opacity = 0.5; // 透明度
+    // material.side = THREE.BackSide; // 背面可见
+    // material.side = THREE.DoubleSide; // 双面可见
+    console.log("material.side:", material.side);
+
+    // 几何体属性
+    console.log("mesh", mesh);
+    console.log("mesh.geometry", mesh.geometry);
+    console.log("mesh.material", mesh.material);
+    // 访问模型材质，并设置材质的颜色属性
+    mesh.material.color.set(0xffff00);
+    // 访问模型几何体，并平移几何体顶点数据
+    mesh.geometry.translate(0, 100, 0);
+
+    // 克隆和复制
+    const v1 = new THREE.Vector3(1, 2, 3);
+    console.log("v1", v1);
+    const v2 = v1.clone();
+    console.log("v2", v2);
+    const v3 = new THREE.Vector3(4, 5, 6);
+    v3.copy(v1);
+    console.log("v3", v3);
+    // Mesh克隆
+    const mesh2 = mesh.clone();
+    mesh2.position.x = 100;
+    // 由于mesh2克隆mesh，二者共用一套material，所以修改一个另一个也会变
+    mesh2.material.color.set(0xffffff);
+    mesh2.geometry = mesh.geometry.clone();
+    mesh2.material = mesh.material.clone();
+    mesh2.material.color.set(0xff0000); // 克隆几何体的材质，并重新设置
+
+    /* 练习 */
+    // 1. 改变mesh2位置，使之位于mesh2正上方，距离100
+    mesh2.position.copy(mesh.position);
+    mesh2.position.y += 100;
+    // 2. 两个模型姿态角度始终保持一致(在render中修改)
+
     // 将立方体添加到场景中
-    scene.add(mesh);
-    return { mesh };
+    scene.add(mesh, mesh2);
+    return { mesh, mesh2 };
   };
 
   /* AxesHelper */
@@ -106,13 +145,17 @@ const BufferGeometry = () => {
     renderer: THREE.WebGLRenderer,
     scene: THREE.Scene,
     camera: THREE.Camera,
-    mesh?: THREE.Mesh
+    mesh?: THREE.Mesh,
+    mesh2?: THREE.Mesh
   ) => {
     const stats = new Stats();
     document.body.appendChild(stats.domElement);
     function render() {
       stats.update();
-      // if (mesh) mesh.rotateY(0.01);
+      if (mesh) {
+        mesh.rotateY(0.01);
+        mesh2 ? mesh2.rotation.copy(mesh.rotation) : null;
+      }
       renderer.render(scene, camera);
       animationFrameId.current = requestAnimationFrame(render);
     }
@@ -156,7 +199,7 @@ const BufferGeometry = () => {
 
     // createPointGeometry(scene);
     // createV3Object(scene);
-    const { mesh } = createEulerObject(scene);
+    const { mesh, mesh2 } = createEulerObject(scene);
 
     /* 创建相机 */
     const camera = createCamera(sizes);
@@ -171,7 +214,7 @@ const BufferGeometry = () => {
     createOrbitControls(scene, camera, renderer);
 
     /* 创建渲染循环 */
-    const render = createRenderLoop(renderer, scene, camera, mesh);
+    const render = createRenderLoop(renderer, scene, camera, mesh, mesh2);
     render();
 
     /* 设置窗口大小改变时的处理函数 */
